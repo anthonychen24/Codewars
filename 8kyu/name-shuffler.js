002caa8608087a59dfaa37cbb674969089fbc6c9
Anthony Chen <98573429+anthonychen24@users.noreply.github.com>
2022-07-15 19:36:09 -0400

    first name swapped with last name

---
+++ 8kyu/name-shuffler.js
@@ -0,0 +1,14 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+// function nameShuffler(str){
+//     //Shuffle It
+//   }
+
+//my solution
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
