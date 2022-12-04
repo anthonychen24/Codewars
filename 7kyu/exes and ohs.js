f3d9f5cf4f07e0df3fa3cfd7ad444fa67c48ff75
Anthony Chen <98573429+anthonychen24@users.noreply.github.com>
2022-12-04 00:04:45 -0500

    Check to see if a string has the same amount of 'x's and 'o's

---
+++ 7kyu/exes and ohs.js
@@ -0,0 +1,15 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+// my solution
+function XO(str) {
+    let newStr = str.toLowerCase().split('')
+    return newStr.filter(x => x=='x').length === newStr.filter(x => x=='o').length
+  }
