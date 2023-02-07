d84ab9ae715e195b925a49a7411000f0e4e75abe
Anthony Chen <98573429+anthonychen24@users.noreply.github.com>
2023-02-07 00:44:54 -0500

    Complete the solution so that it returns a formatted string.

---
+++ 7kyu/Substituting Variables Into Strings Padded Numbers.js
@@ -0,0 +1,8 @@
+// Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+// Example:
+
+// solution(5) // should return "Value is 00005"
+
+// my solution
+const solution = value => 'Value is ' + value.toString().padStart(5, '0')
