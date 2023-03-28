9ead5efbac225264c8d37f9148a5b1bc8fb5a268
Anthony Chen <98573429+anthonychen24@users.noreply.github.com>
2023-03-28 00:37:44 -0400

    Bob is a lazy man.

---
+++ 7kyu/Help Bob count letters and digits.js
@@ -0,0 +1,14 @@
+// Bob is a lazy man.
+
+// He needs you to create a method that can determine how many letters (both uppercase and lowercase ASCII letters) and digits are in a given string.
+
+// Example:
+
+// "hel2!lo" --> 6
+
+// "wicked .. !" --> 6
+
+// "!?..A" --> 1
+
+// my solution
+const countLettersAndDigits = input => input.replace(/[^0-9a-z]/gi, '').length
