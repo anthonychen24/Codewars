f568f17ed952c240dee1196be27da30b23ca57da
Anthony Chen <98573429+anthonychen24@users.noreply.github.com>
2023-04-17 21:04:15 -0400

    returns the number of times the search_text is found

---
+++ 7kyu/return substring instance count.js
@@ -0,0 +1,9 @@
+// Complete the solution so that it returns the number of times the search_text is found within the full_text.
+
+// Usage example:
+
+// solution('aa_bb_cc_dd_bb_e', 'bb') # should return 2 since bb shows up twice
+// solution('aaabbbcccc', 'bbb') # should return 1
+
+// my solution
+const solution = (fullText, searchText) => fullText.split(searchText).length-1
